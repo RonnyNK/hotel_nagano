fccf016167175873ff92e5f13a1a7ffbe67d815e
Ronny Nou-Khlot <ronny.nou-khlot@live.com>
2021-03-18 15:27:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelNagano::Application.config.secret_token = 'e415576b174b15c922c291252deedb47d0c2560989dbc263fefb0980321cdfeaee839c8b4e02620891bcf25e4013df6ad733a102e3ffd5a43046de3aa364f0d3'
